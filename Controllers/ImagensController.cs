58a8b89dcebf8864df466c3a7da415a6392097d8
Gustavo Russo <gustavorussodecamargo10@hotmail.com>
2024-05-20 13:51:14 -0300

    PostTeste

---
+++ Controllers/ImagensController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioSintoniaPreview.Controllers
+{
+    public class ImagensController : Controller
+    {
+        private string caminhoServidor;
+
+        public ImagensController(IWebHostEnvironment Sistema)
+        {
+            caminhoServidor = Sistema.WebRootPath;
+        }
+
+        public IActionResult UploadImagem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadImagem(IFormFile arquivo)
+        {
+            string caminhoParaSalvarArquivo = caminhoServidor + "\\arquivos\\";
+            string novoNomeParaArquivo = Guid.NewGuid().ToString() + "_" +  arquivo.FileName;
+            if ( ! Directory.Exists(caminhoParaSalvarArquivo))
+            {
+                Directory.CreateDirectory(caminhoParaSalvarArquivo);
+            }
+            using (var stream = System.IO.File.Create(caminhoParaSalvarArquivo + novoNomeParaArquivo)) 
+            {
+                arquivo.CopyToAsync(stream);
+            }
+            return RedirectToAction("UploadImagem");
+        }
+    }
+}
